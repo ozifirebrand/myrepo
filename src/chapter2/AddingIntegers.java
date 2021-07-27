d5b19b24a3fff29b1b6c5f1c7e448b389e2fdcc3
Ozi <oziomaokoroafor@gmail.com>
2021-07-27 13:15:57 +0100

    Adding integers

--- src/chapter2/AddingIntegers.java
+++ src/chapter2/AddingIntegers.java
@@ -1,4 +1,24 @@
-package chapter2;
+/*PSEUDOCODE
+Prompt user to input first number
+User inputs number
+Prompt user to input second number
+User inputs number
+Compute sum as the sum of the two integers
+Display the sum.
+End program.
 
+ */
+package chapter2;
+import java.util.Scanner;
 public class AddingIntegers {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in first number below ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Type in second number below ");
+        int secondNumber = scanner.nextInt();
+        System.out.println("Sum of the two integers is " +(firstNumber+secondNumber));
+
+
+    }
 }
